182b45dc92c300925ec7572ccbed6318ce5c1c58
bancerk <192424850+bancerk@users.noreply.github.com>
2025-05-10 03:38:45 +0300

    user and customer menu implementation (11:51)

--- src/main/java/PatikaStoreMain.java
+++ src/main/java/PatikaStoreMain.java
@@ -1,27 +1,41 @@
 import exception.PatikaStoreException;
+import model.User;
+import model.enums.Role;
 import service.CustomerService;
+import service.UserService;
+import util.PasswordUtil;
 
 import java.util.Scanner;
 
 public class PatikaStoreMain {
 
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private static final UserService userService = new UserService();
+
+
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        String choice = scanner.nextLine();
 
         while (true) {
-            System.out.println("====Patika Store Hoş Geldiniz===");
-            System.out.println("1 - Müşteri Kaydı");
-            System.out.println("2 - Giriş Yap");
-            System.out.println("0 - Çıkış");
 
-            System.out.print("Seçim yapınız: ");
-            String choice = scanner.nextLine();
+            getMainMenu();
+
+
+
+
+
+//            System.out.println("====Patika Store Hoş Geldiniz===");
+//            System.out.println("1 - Müşteri Kaydı");
+//            System.out.println("2 - Giriş Yap");
+//            System.out.println("0 - Çıkış");
 
             try {
                 switch (choice) {
                     case "1":
-                        saveCustomer(scanner);
+                        getUserLoginMenu();
+//                        saveCustomer(scanner);
                         break;
                     case "2":
                         loginCustomer(scanner);
@@ -38,17 +52,64 @@ public class PatikaStoreMain {
         }
     }
 
+    private static void getUserLoginMenu() {
+        while(true) {
+            System.out.println("==== KULLANICI GİRİŞ PANELİ ===");
+            System.out.println("1 - Kullanıcı Kayıt Ol");
+            System.out.println("1 - Kullanıcı Girişi");
+            System.out.println("0 - Geri Dön");
+            System.out.print("Seçim yapınız: ");
+
+            switch (choice){
+                case "1":
+                    registerUser();
+                    break;
+                case "2":
+                    loginUser();
+                    break;
+                case 0:
+                    return;
+                default:
+                    System.out.println("Geçersiz Seçim!");
+            }
+        }
+    }
+
+    private static void loginUser() {
+        System.out.print("Kullanıcı Adı: ");
+        String userName = scanner.nextLine();
+        System.out.print("Şifre: ");
+        String password = scanner.nextLine();
+        System.out.print("Rol seçiniz: ADMIN-SUPPORT");
+        String roleString = scanner.nextLine().toUpperCase();
+
+        Role role = Role.valueOf(roleString);
+        User user = new User(userName, PasswordUtil.hash(password),role);
+    }
+
+    private static void registerUser() {
+
+    }
+
+    private static void getMainMenu() {
+        System.out.println("==== GİRİŞ TÜRÜ SEÇİN ===");
+        System.out.println("1 - Kullanıcı Girişi (ADMIN,SUPPORT)");
+        System.out.println("1 - Müşteri Girişi");
+        System.out.println("0 - Çıkış");
+        System.out.print("Seçim yapınız: ");
+    }
+
     private static void loginCustomer(Scanner scanner) throws PatikaStoreException {
         System.out.print("Email: ");
-        String email = scanner.nextLine();
+        String email = PatikaStoreMain.scanner.nextLine();
         System.out.println("Şifre: ");
-        String password = scanner.nextLine();
+        String password = PatikaStoreMain.scanner.nextLine();
 
         CustomerService customerService = new CustomerService();
         customerService.login(email,password);
     }
 
-    private static void saveCustomer(Scanner scanner) throws PatikaStoreException {
+    private static void saveCustomer() throws PatikaStoreException {
         System.out.print("İsim: ");
         String name = scanner.nextLine();
         System.out.print("Email: ");
